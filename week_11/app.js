aaa8edbb5c786c12c67e89942a90565580c54965
EC2 Default User <ec2-user@ip-172-31-4-50.ec2.internal>
2020-12-08 20:01:20 +0000

    uploading parts of final app

--- week_11/app.js
+++ week_11/app.js
@@ -6,10 +6,10 @@ const moment = require('moment-timezone');
 const handlebars = require('handlebars');
 var fs = require('fs');
 
-const indexSource = fs.readFileSync("sensordata.html").toString();
+const indexSource = fs.readFileSync("templates/sensor.txt").toString();
 var template = handlebars.compile(indexSource, { strict: true });
 
-const pbSource = fs.readFileSync("processblog.html").toString();
+const pbSource = fs.readFileSync("templates/pb.txt").toString();
 var pbtemplate = handlebars.compile(pbSource, { strict: true });
 
 // AWS RDS credentials
@@ -17,8 +17,9 @@ var db_credentials = new Object();
 db_credentials.user = 'zhurk746';
 db_credentials.host = 'data-structures.cjoiyyanmepr.us-east-1.rds.amazonaws.com'; 
 db_credentials.database = 'aa';
-db_credentials.password = 'ta143hoe'
+db_credentials.password = 'ta143hoe';
 db_credentials.port = 5432;
+//db_credentials.mapbox= 'pk.eyJ1Ijoidm9ucmFtc3kiLCJhIjoiY2pveGF1MmxoMjZnazNwbW8ya2dsZTRtNyJ9.mJ1kRVrVnwTFNdoKlQu_Cw';
 
 // create templates
 var hx = `<!doctype html>
@@ -32,10 +33,10 @@ var hx = `<!doctype html>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
        integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
        crossorigin=""/>
-       <style >
-     #mapid {position:absolute; top: 0; bottom: 0; left: 0; right: 0;}
-   </style>
 </head>
+<style>
+#mapid { height: 760px; }
+</style>
 <body>
 <div id="mapid"></div>
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
@@ -56,7 +57,8 @@ var jx = `;
         accessToken: 'pk.eyJ1Ijoidm9ucmFtc3kiLCJhIjoiY2pveGF1MmxoMjZnazNwbW8ya2dsZTRtNyJ9.mJ1kRVrVnwTFNdoKlQu_Cw'
     }).addTo(mymap);
     for (var i=0; i<data.length; i++) {
-        L.marker( [data[i].mtgLat, data[i].mtgLong] ).bindPopup(JSON.stringify(data[i].meetings)).addTo(mymap);
+        L.marker([data[i].mtglat, data[i].mtglong]).bindPopup(JSON.stringify(data[i].meetings)).addTo(mymap);
+        console.log(data[2])
     }
     </script>
     </body>
@@ -73,19 +75,17 @@ app.get('/aa', function(req, res) {
     var now = moment.tz(Date.now(), "America/New_York"); 
     var dayy = now.day().toString(); 
     var hourr = now.hour().toString(); 
-    var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
-    "Friday", "Saturday"
-  ];
-  var dayy2 = weekdays[dayy];
+    var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"];
+    var dayy2 = weekdays[dayy];
 
 
     // Connect to the AWS RDS Postgres database
     const client = new Pool(db_credentials);
     
     // SQL query 
-    var thisQuery = `SELECT mtgLat, mtgLong, json_agg(json_build_object('loc', mtgPlace, 'address', mtgAddress, 'name', mtgName, 'day', mtgDay, 'types', mtgTypeOf, 'time', mtgStartTime)) as meetings
+    var thisQuery = `SELECT mtgLat, mtgLong, json_agg(json_build_object('loc', mtgPlace, 'address', mtgAddress, 'name', mtgName, 'day', mtgDay, 'types', mtgTypeOf, 'shour', mtgStartTime)) as meetings
                  FROM aaMeetings 
-                 WHERE mtgDay = ` + dayy2 + 
+                 WHERE mtgDay = ` + '\'' + dayy2 + '\'' + 
                  `GROUP BY mtgLat, mtgLong
                  ;`;
 
@@ -108,7 +108,7 @@ app.get('/temperature', function(req, res) {
 
     // SQL query 
     var q = `SELECT EXTRACT(DAY FROM sensorTime) as sensorday,
-             AVG(sensorValue::int) as num_obs
+             AVG(sensorValue/100::int) as num_obs
              FROM sensorData
              GROUP BY sensorday
              ORDER BY sensorday;`;
@@ -131,31 +131,42 @@ app.get('/processblog', function(req, res) {
 
     // Connect to the AWS DynamoDB database
     var dynamodb = new AWS.DynamoDB();
-    //const async = require('async');
+    const async = require('async');
 
-    //var blogEntries=["1","2","3","4","6","7","8","9","10","11"];
-    // DynamoDB (NoSQL) query
-    //async.eachSeries(blogEntries, function(value,callback) {
-    var params = {
-        TableName : "restaurantblog",
-        KeyConditionExpression: "pk= :pk", // the query expression
-        ExpressionAttributeValues: { // the query values
-            ":pk": {N: "1".toString()}
-        }
-    };
+    var blogEntries=["1","2","3","4","6","7","8","9","10","11","12","13","14","15", "16", "17"]
+    var queryResults = []; // array to hold all the posts we retrieve
 
-    dynamodb.query(params, function(err, data) {
-        if (err) {
-            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-            throw (err);
-        }
-        else {
-            console.log(data.Items)
-            res.end(pbtemplate({ pbdata: JSON.stringify(data.Items)}));
-            console.log('3) responded to request for process blog data');
-        }
-    });
-    //callback();
+    async.eachSeries(blogEntries, function(entryID, callback){
+        // 
+        // this function gets run once for each of the entry IDs in `blogEntries`
+        // 
+        var params = {
+            TableName : "restaurantblog",
+            KeyConditionExpression: "pk= :pk", // the query expression
+            ExpressionAttributeValues: { // the query values
+                ":pk": {N: entryID.toString()} // request a particular post
+            }
+        };
+
+        dynamodb.query(params, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                throw (err);
+            }
+            else {
+                // add the info for this post to the array collecting all our results
+                queryResults.push(data.Items)
+            }
+            callback(); // move on to the next entryID
+        });
+    }, function(){
+        // 
+        // this function gets called once after all the queries have been completed
+        // 
+        console.log("retrieved:", queryResults)
+        res.end(pbtemplate({ pbdata: JSON.stringify(queryResults)}));
+    })
+
 });
 
 //serve static files in /public
